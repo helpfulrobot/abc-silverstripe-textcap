93cd7cd4f336d62c826c0794459b4ce4fcf95718
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:41:40 +1300

    Converted to PSR-2

--- code/AbcTextCap.php
+++ code/AbcTextCap.php
@@ -1,69 +1,75 @@
 <?php
 
-class AbcTextCap {
+class AbcTextCap
+{
 
-	/**
+    /**
      * @config
      */
     private static $text_captcha_api_key = null;
 
-	/**
-	 * Sets the API Key
-	 * @param [type] $key [description]
-	 */
-	public static function set_text_captcha_api_key($key = null) {
-		static::$text_captcha_api_key = $key;
-	}
+    /**
+     * Sets the API Key
+     * @param [type] $key [description]
+     */
+    public static function set_text_captcha_api_key($key = null)
+    {
+        static::$text_captcha_api_key = $key;
+    }
 
-	/**
-	 * [getCapData description]
-	 */
-	public static function getCapData(){
+    /**
+     * [getCapData description]
+     */
+    public static function getCapData()
+    {
 
-		// API Key
-		$key = Config::inst()->get('AbcTextCap', 'text_captcha_api_key');
-		if (!$key) $key = uniqid();
+        // API Key
+        $key = Config::inst()->get('AbcTextCap', 'text_captcha_api_key');
+        if (!$key) {
+            $key = uniqid();
+        }
 
-		// try to use the text captcha service
-		$response = file_get_contents('http://api.textcaptcha.com/' . $key . '.xml');
-		if ($response) {
-			$document = new DOMDocument();
-			if ($document->loadXML($response)) {
-				$data = (object) array(
-					'q'	=> $entries = $document->getElementsByTagName('question')->item(0)->nodeValue,
-					'a'	=> array()
-				);
-				$answers = $document->getElementsByTagName('answer');
-				foreach ($answers as $a) {
-					$data->a[] = $a->nodeValue;
-				}
+        // try to use the text captcha service
+        $response = file_get_contents('http://api.textcaptcha.com/' . $key . '.xml');
+        if ($response) {
+            $document = new DOMDocument();
+            if ($document->loadXML($response)) {
+                $data = (object) array(
+                    'q'    => $entries = $document->getElementsByTagName('question')->item(0)->nodeValue,
+                    'a'    => array()
+                );
+                $answers = $document->getElementsByTagName('answer');
+                foreach ($answers as $a) {
+                    $data->a[] = $a->nodeValue;
+                }
+            }
+        }
 
-			}
-		}
+        // fall back to the internal "database" of questions
+        if (empty($data->q) || empty($data->a) || (Session::get("AbcTextCap") && $data->q == Session::get("AbcTextCap")->q)) {
+            $data = (object) AbcTextCapData::getChallenge();
+            $as = array();
+            foreach ($data->a as $a) {
+                $as[] = md5(strtolower(trim($a)));
+            }
+            $data->a = $as;
+        }
 
-		// fall back to the internal "database" of questions
-		if (empty($data->q) || empty($data->a) || (Session::get("AbcTextCap") && $data->q == Session::get("AbcTextCap")->q) ){
-			$data = (object) AbcTextCapData::getChallenge();
-			$as = array();
-			foreach ($data->a as $a) {
-				$as[] = md5(strtolower(trim($a)));
-			}
-			$data->a = $as;
-		}
+        // save the cap data
+        Session::set("AbcTextCap", $data);
 
-		// save the cap data
-		Session::set("AbcTextCap", $data);
+        // return
+        return $data;
+    }
 
-		// return
-		return $data;
-	}
-
-	public static function validate($answer) {
-		$data = Session::get("AbcTextCap");
-		foreach ($data->a as $a) {
-			if (md5(strtolower(trim($answer))) == $a) return true;
-		}
-		return false;
-	}
-
+    public static function validate($answer)
+    {
+        $data = Session::get("AbcTextCap");
+        foreach ($data->a as $a) {
+            if (md5(strtolower(trim($answer))) == $a) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- code/AbcTextCapField.php
+++ code/AbcTextCapField.php
@@ -1,43 +1,49 @@
 <?php
 
-class AbcTextCapField extends TextField {
+class AbcTextCapField extends TextField
+{
 
-	protected $capData = null;
-	protected $template	= 'AbcTextCapField';
-	protected $isValid = null;
+    protected $capData = null;
+    protected $template    = 'AbcTextCapField';
+    protected $isValid = null;
 
-	public function __construct($name, $title = null, $value = '', $maxLength = null, $form = null) {
-		parent::__construct($name, $title, $value, $maxLength, $form);
-	}
+    public function __construct($name, $title = null, $value = '', $maxLength = null, $form = null)
+    {
+        parent::__construct($name, $title, $value, $maxLength, $form);
+    }
 
-	public function getQuestion(){
-		if (empty($this->capData)) $this->capData = AbcTextCap::getCapData();
-		return $this->capData->q;
-	}
+    public function getQuestion()
+    {
+        if (empty($this->capData)) {
+            $this->capData = AbcTextCap::getCapData();
+        }
+        return $this->capData->q;
+    }
 
-	public function validate($validator) {
+    public function validate($validator)
+    {
 
-		// extract the value
-		$value = empty($this->value) ? $_REQUEST[$this->name] : $this->value;
+        // extract the value
+        $value = empty($this->value) ? $_REQUEST[$this->name] : $this->value;
 
-		// validate and save the validation result in case validate gets called twice
-		if ($this->isValid === null) $this->isValid = AbcTextCap::validate($value);
+        // validate and save the validation result in case validate gets called twice
+        if ($this->isValid === null) {
+            $this->isValid = AbcTextCap::validate($value);
+        }
 
-		// push validation error
-		if (!$this->isValid) {
-			$validator->validationError(
-				$this->name,
-				'You answered the question incorrectly',
-				"validation",
-				false
-			);
-		}
+        // push validation error
+        if (!$this->isValid) {
+            $validator->validationError(
+                $this->name,
+                'You answered the question incorrectly',
+                "validation",
+                false
+            );
+        }
 
-		// reset the question
-		$this->capData = AbcTextCap::getCapData();
+        // reset the question
+        $this->capData = AbcTextCap::getCapData();
 
-		return $this->isValid;
-
-	}
-
+        return $this->isValid;
+    }
 }
